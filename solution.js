6fcc8f57139f2a9efa03594a0132e4a577c7aedd
Victor Lee <myleevictor@gmail.com>
2018-03-17 17:35:52 +0800

    refactored solution to encode/decode only characters that are between a-b..A-B on the ASCII table leaving untouched whitespaces and special characters

--- solution.js
+++ solution.js
@@ -1,18 +1,37 @@
-var encrypt = function(string, shiftAmount) {
-  // YOUR SOLUTION HERE
+function caesarCipher(string, shiftAmount) {
+  return string
+    .split("")
+    .map(char => {
+      if (
+        char.toLowerCase() !== char.toUpperCase() &&
+        char.charCodeAt(0) !== 32
+      ) {
+        let baseIndex = char >= "a" ? 97 : 65;
+        let newIndex = char.charCodeAt(0) - baseIndex + shiftAmount;
+        return String.fromCharCode(baseIndex + (newIndex + 26) % 26);
+      } else {
+        return char;
+      }
+    })
+    .join("");
 }
+
+var encrypt = function(string, shiftAmount) {
+  return caesarCipher(string, shiftAmount % 26);
+};
 
 var decrypt = function(string, shiftAmount) {
-  // YOUR SOLUTION HERE
-}
-
-
+  return caesarCipher(string, (-shiftAmount + 26) % 26);
+};
 
 // DO NOT TOUCH THIS PART OF THE CODE!
-if (process.argv[2] === "Hgx Angwkxw Hkvatkw Khtw, Vhgvhkwx Ahmxe & Lahiibgz Ftee, #sxkhmph-yhkmrmakxx") {
-	console.log(decrypt(process.argv[2], 19))
+if (
+  process.argv[2] ===
+  "Hgx Angwkxw Hkvatkw Khtw, Vhgvhkwx Ahmxe & Lahiibgz Ftee, #sxkhmph-yhkmrmakxx"
+) {
+  console.log(decrypt(process.argv[2], 19));
 }
 module.exports = {
-	encrypt: encrypt,
-	decrypt: decrypt
-}
+  encrypt: encrypt,
+  decrypt: decrypt
+};
